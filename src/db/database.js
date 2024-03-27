a591fe276e404c74bd7d856d50a8e7973abf202b
Suhanii1214 <suhani.12010153@lpu.in>
2024-03-27 12:50:33 +0530

    added user and video model

--- src/db/database.js
+++ src/db/database.js
@@ -3,8 +3,8 @@ import { DB_NAME } from "../constants.js";
 
 const dbConnect = async () => {
     try {
-        const connectionInstance = mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-        console.log(`MongoDB Connection SUCCESSFULL!`);
+        const connectionInstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        console.log(`MongoDB Connection SUCCESSFULL! ${connectionInstance.connection.host}`);
     } catch (error) {
         console.log(`MongoDB Connection FAILED: ${error}`);
         process.exit(1)
--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-import express from "express"
 import dotenv from 'dotenv/config'
 import dbConnect from "./db/database.js"
+import { app } from "./app.js"
 
-const app = express()
-
-app.listen(process.env.PORT, () => {
-    dbConnect()
-    console.log(`Server running on port ${process.env.PORT}`);
+dbConnect()
+.then(() => {
+    app.listen(process.env.PORT, () => {
+        console.log(`Server running on port ${process.env.PORT}`);
+    })
+}).catch((err) => {
+    console.log(`MongoDB Connection FAILED! ${err}`);
 })
